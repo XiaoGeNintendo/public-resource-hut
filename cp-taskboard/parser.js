e0afe7f9ff3591d79dd792a77b370b21730e0d35
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2020-03-10 14:43:43 +0800

    Update parser.js

--- cp-taskboard/parser.js
+++ cp-taskboard/parser.js
@@ -1,5 +1,5 @@
 parser={
-    ver:"0.0.1",
+    ver:"0.0.2",
     parser:[
         {
             name:"Codeforces", //oj name
@@ -9,6 +9,23 @@ parser={
                 for(var i=0;i<vs.length;i++){
                     if(vs[i]=="problem"){
                         return vs[i+1]+nqoa(vs[i+2])
+                    }
+                }
+
+                return null
+            },
+            getTitle:function(){ //the function for getting title. Null for invalid problem.
+                return document.getElementsByClassName("title")[0].innerText;
+            }
+        },
+        {
+            name:"Codeforces", //oj name
+            regex:".*.codeforces.com/contest/.*/problem/.*", //check site
+            getId:function(){ //the function for getting id. UID=oj name+"_"+id. If null is returned, the problem won't be considered valid
+                var vs=location.href.split("/");
+                for(var i=0;i<vs.length;i++){
+                    if(vs[i]=="contest"){
+                        return vs[i+1]+nqoa(vs[i+3])
                     }
                 }
 
