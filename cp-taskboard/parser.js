93f32de960d09037a40a74305a97872b3b3e1c41
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2020-03-10 18:25:54 +0800

    Update parser.js

--- cp-taskboard/parser.js
+++ cp-taskboard/parser.js
@@ -1,14 +1,14 @@
 parser={
-    ver:"0.0.2",
+    ver:"0.0.3",
     parser:[
         {
             name:"Codeforces", //oj name
-            regex:".*.codeforces.com/problemset/problem/.*/.*", //check site
+            regex:`.*codeforces\.com\/problemset\/problem\/.*\/.*`, //check site
             getId:function(){ //the function for getting id. UID=oj name+"_"+id. If null is returned, the problem won't be considered valid
                 var vs=location.href.split("/");
                 for(var i=0;i<vs.length;i++){
                     if(vs[i]=="problem"){
-                        return vs[i+1]+nqoa(vs[i+2])
+                        return vs[i+1]+nqoa(vs[i+2]).toUpperCase()
                     }
                 }
 
@@ -20,12 +20,12 @@ parser={
         },
         {
             name:"Codeforces", //oj name
-            regex:".*.codeforces.com/contest/.*/problem/.*", //check site
+            regex:`.*codeforces\.com\/(contest|gym)\/.*\/problem\/.*`, //check site
             getId:function(){ //the function for getting id. UID=oj name+"_"+id. If null is returned, the problem won't be considered valid
                 var vs=location.href.split("/");
                 for(var i=0;i<vs.length;i++){
-                    if(vs[i]=="contest"){
-                        return vs[i+1]+nqoa(vs[i+3])
+                    if(vs[i]=="contest" || vs[i]=="gym"){
+                        return vs[i+1]+nqoa(vs[i+3]).toUpperCase()
                     }
                 }
 
@@ -34,6 +34,42 @@ parser={
             getTitle:function(){ //the function for getting title. Null for invalid problem.
                 return document.getElementsByClassName("title")[0].innerText;
             }
-        }
+        },
+        {
+            name:"Atcoder", //oj name
+            regex:`.*atcoder\.jp\/contests\/.*\/tasks\/.*`, //check site
+            getId:function(){ //the function for getting id. UID=oj name+"_"+id. If null is returned, the problem won't be considered valid
+                var vs=location.href.split("/");
+                for(var i=0;i<vs.length;i++){
+                    if(vs[i]=="contests"){
+                        return vs[i+1]+"-"+nqoa(vs[i+3])
+                    }
+                }
+
+                return null
+            },
+            getTitle:function(){ //the function for getting title. Null for invalid problem.
+                return document.getElementsByClassName("h2")[0].innerText;
+            }
+        },
+        {
+            name:"Atcoder", //oj name
+            regex:`.*\.contest\.atcoder\.jp\/tasks\/.*`, //check site
+            getId:function(){ //the function for getting id. UID=oj name+"_"+id. If null is returned, the problem won't be considered valid
+                var vs=location.href.split("/");
+                for(var i=0;i<vs.length;i++){
+                    if(vs[i].includes("contest")){
+                        return vs[i].split(".")[0]+"-"+nqoa(vs[i+2])
+                    }
+                }
+
+                return null
+            },
+            getTitle:function(){ //the function for getting title. Null for invalid problem.
+                return document.getElementsByClassName("h2")[0].innerText;
+            }
+        },
+
+        
     ]
 }
